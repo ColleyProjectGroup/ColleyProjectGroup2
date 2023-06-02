5ddccab079343ec0067fbc918dbf1d004d7a7692
hookor <gerrard1219@gmail.com>
2023-06-03 01:52:22 +0900

    Directory set-up

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>Initial Setup</div>;
+};
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Alias } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
-    alias: [{ find: '@', replacement: `${__dirname}/src` }],
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+
+      pages: `${path.resolve(__dirname, './src/pages/')}`,
+      components: `${path.resolve(__dirname, './src/components/')}`,
+      services: `${path.resolve(__dirname, './src/services/')}`,
+      hooks: `${path.resolve(__dirname, './src/hooks/')}`,
+      contexts: `${path.resolve(__dirname, './src/contexts/')}`,
+      styles: `${path.resolve(__dirname, './src/styles/')}`,
+      types: `${path.resolve(__dirname, './src/types/')}`,
+      utils: `${path.resolve(__dirname, './src/utils/')}`,
+    },
   },
 });
