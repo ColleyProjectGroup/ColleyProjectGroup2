3ce9ddc644b09840b551004ad338c1f0e7e106bd
EungBug <eungb14@gmail.com>
2023-06-03 18:16:59 +0900

    Docs: Axios 기본 설정 추가

---
+++ src/api/axios.ts
@@ -0,0 +1,52 @@
+import axios, { AxiosError, AxiosInstance } from 'axios';
+
+const authInterceptors = (instance: AxiosInstance): AxiosInstance => {
+  instance.interceptors.request.use(
+    config => {
+      // 로컬스토리지에 저장 되어 있는 AccessToken을 가져온다.
+      const accessToken = localStorage.getItem(import.meta.env.VITE_STORAGE_KEY_ACCESSTOKEN);
+      if (config.headers && accessToken) {
+        // AccessToken이 정상적으로 저장되어 있으면 headers에 Authorization에 값을 추가해준다.
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+      // authorization을 추가한 config 반환
+      return config;
+    },
+    (error: AxiosError): Promise<AxiosError> => {
+      return Promise.reject(error);
+    }
+  );
+
+  return instance;
+};
+
+// Authorization 설정이 없는 일반 사용자 API용 Instance
+export const baseInstance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    apikey: import.meta.env.VITE_APIKEY,
+    username: import.meta.env.VITE_USERNAME
+  }
+});
+
+// 관리자 API용 Instance
+export const adminInstance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    apikey: import.meta.env.VITE_APIKEY,
+    username: import.meta.env.VITE_USERNAME,
+    masterKey: true
+  }
+});
+
+// Authorization 설정이 추가된 로그인한 사용자 API용 Instance
+export const authInstance: AxiosInstance = authInterceptors(baseInstance);
+
+////////////////////////////////
+// * Axios Instance 기본 사용법
+// * 1. 필요한 instance import
+// * 2. instance.method({API_PATH}, {필요한 경우 Body})
+// adminInstance.get('/products');
+// authInstance.post('/auth/me');
+// baseInstance.post('/auth/login', { email: '', password: ''});
+////////////////////////////////
