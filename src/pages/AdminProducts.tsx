8085ad38dfb945a8064439fef5d264c98c99da3d
EungBug <eungb14@gmail.com>
2023-06-08 17:04:54 +0900

    Feat: Product title Search 추가

--- src/pages/AdminProducts.tsx
+++ src/pages/AdminProducts.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useCallback } from 'react'
+import { useEffect, useState, useRef, useCallback, useMemo } from 'react'
 import { AdminProductItem, AdminProductItemHeader } from 'components/index'
 import styled from 'styles/pages/adminProducts.module.scss'
 import { Link } from 'react-router-dom'
@@ -7,8 +7,13 @@ import { Product } from 'types/index'
 import { useOutsideClick } from 'hooks/index'
 
 export const AdminProducts = () => {
+  const [search, setSearch] = useState<string>('')
   const [products, setProducts] = useState<Array<Product>>([])
   const [shownMenuId, setShownMenuId] = useState<string | null>(null)
+  const filteredProducts = useMemo(
+    () => products.filter(product => product.title.includes(search)),
+    [products, search]
+  )
   const addButtonRef = useRef<HTMLButtonElement | null>(null)
   useEffect(() => {
     adminFetchProducts().then(res => {
@@ -29,6 +34,13 @@ export const AdminProducts = () => {
     setShownMenuId('')
   }, [])
 
+  const onChangeSearch = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(event.target.value.trim())
+    },
+    []
+  )
+
   return (
     <section className={styled['admin-content-wrapper']}>
       <h1 className={styled['admin-title']}>상품 관리</h1>
@@ -39,8 +51,15 @@ export const AdminProducts = () => {
           상품 추가
         </button>
       </Link>
+      <input
+        className={styled.search}
+        type="text"
+        placeholder="상품명 입력"
+        value={search}
+        onChange={onChangeSearch}
+      />
       <AdminProductItemHeader />
-      {products.map(product => {
+      {filteredProducts.map(product => {
         const isMenuShow = shownMenuId === product.id
         return (
           <AdminProductItem
