93de509ec71aea4a6175dad0a9f8e7d7fc2a0c87
EungBug <eungb14@gmail.com>
2023-06-13 22:09:48 +0900

    Feat: 관리자 고객관리 목록 조회 구현

--- src/components/admin/AdminCustomerItem.tsx
+++ src/components/admin/AdminCustomerItem.tsx
@@ -8,14 +8,36 @@ export const AdminCustomerItem = ({
   totalTransactionPrice
 }: CustomerInfo) => {
   const customerGrade = useMemo(() => {
-    // 브론즈 > 실버 > 골드 > 플래티넘 > 다이아몬드
-  }, [user, totalTransactionPrice])
+    // 회원등급 (누적금액)
+    // 브론즈(0~10만) > 실버(10만~20만) > 골드(20만~50만) > 플래티넘(50만~100만) > 다이아몬드(100만~)
+    if (totalTransactionPrice < 100000) {
+      return '브론즈'
+    } else if (
+      totalTransactionPrice >= 100000 &&
+      totalTransactionPrice < 200000
+    ) {
+      return '실버'
+    } else if (
+      totalTransactionPrice >= 200000 &&
+      totalTransactionPrice < 500000
+    ) {
+      return '골드'
+    } else if (
+      totalTransactionPrice >= 500000 &&
+      totalTransactionPrice < 1000000
+    ) {
+      return '플래티넘'
+    } else if (totalTransactionPrice >= 1000000) {
+      return '다이아몬드'
+    }
+    return '-'
+  }, [totalTransactionPrice])
 
   return (
     <li className={styled['customer']}>
       <div className={styled['customer__email']}>{user.email}</div>
       <div className={styled['customer__name']}>{user.displayName}</div>
-      <div className={styled['customer__grade']}>{'실버'}</div>
+      <div className={styled['customer__grade']}>{customerGrade}</div>
       <div className={styled['customer__total-order']}>
         {totalTransaction.toLocaleString()}건
       </div>
