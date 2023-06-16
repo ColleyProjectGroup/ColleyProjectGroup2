88051ccf179ddf12ee1051ef0892688c3492377d
donghae <todonghae@gmail.com>
2023-06-16 10:40:43 +0900

    BestSeller 컴포넌트, Products 컴포넌트 네트워크 무한 요청 문제 수정

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { adminInstance } from '../api/axios'
 import '../styles/layout/NewArrival.scss'
 
@@ -15,10 +15,10 @@ interface Product {
 }
 
 interface ProductsProps {
-  tagFilter?: string[]
-  limit?: number
-  sortOption?: string | null
-  getProductCount: (count: number) => void // 수정: 상품 개수를 매개변수로 받도록 수정
+  tagFilter?: string[] // 상품 카테고리 태그
+  limit?: number // 상품 렌더링 제한 수
+  sortOption?: string | null // 상품 정렬 옵션
+  getProductCount: (count: number) => void // 상품 개수
 }
 
 const Products = ({
@@ -29,55 +29,59 @@ const Products = ({
 }: ProductsProps) => {
   const [products, setProducts] = useState<Product[]>([])
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const query = API_ENDPOINT
+  const fetchProducts = useCallback(async () => {
+    try {
+      const query = API_ENDPOINT
 
-        const response = await adminInstance.get(query)
-        let filteredProducts = response.data
+      const response = await adminInstance.get(query)
+      let filteredProducts = response.data
 
-        if (tagFilter.length > 0) {
-          filteredProducts = filteredProducts.filter(
-            (product: { tags: string | string[] }) =>
-              tagFilter.every(tag => product.tags.includes(tag))
-          )
-        }
-
-        if (sortOption) {
-          filteredProducts.sort((a: Product, b: Product) => {
-            if (sortOption === 'priceLow') {
-              return a.price - b.price
-            } else if (sortOption === 'priceHigh') {
-              return b.price - a.price
-            } else if (sortOption === 'name') {
-              return a.title.localeCompare(b.title, 'ko', {
-                sensitivity: 'base'
-              })
-            } else {
-              return 0
-            }
-          })
-        }
-
-        if (limit) {
-          filteredProducts = filteredProducts.slice(0, limit)
-        }
-
-        setProducts(filteredProducts)
-        getProductCount(filteredProducts.length) // 수정: 상품 개수를 매개변수로 전달하여 호출
-      } catch (error) {
-        console.error('상품 조회 오류 발생', error)
+      if (tagFilter.length > 0) {
+        // 해당 태그를 포함하는 상품 필터링
+        filteredProducts = filteredProducts.filter((product: Product) =>
+          tagFilter.every(tag => product.tags.includes(tag))
+        )
       }
-    }
 
-    fetchProducts()
+      if (sortOption) {
+        // 정렬 옵션이 존재할 경우
+        filteredProducts.sort((a: Product, b: Product) => {
+          if (sortOption === 'priceLow') {
+            return a.price - b.price // 낮은 가격 순
+          } else if (sortOption === 'priceHigh') {
+            return b.price - a.price // 높은 가격 순
+          } else if (sortOption === 'name') {
+            return a.title.localeCompare(b.title, 'ko', {
+              sensitivity: 'base'
+            }) // 상품명을 기준으로 가나다 순으로 정렬
+          } else {
+            return 0
+          }
+        })
+      }
+
+      if (limit) {
+        // 상품 제한 수량
+        filteredProducts = filteredProducts.slice(0, limit)
+      }
+
+      setProducts(filteredProducts)
+      getProductCount(filteredProducts.length)
+      // 렌더링 상품 수 함수 호출
+    } catch (error) {
+      console.error('상품 조회 오류 발생', error)
+    }
   }, [tagFilter, limit, sortOption, getProductCount])
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
 
   const calculateDiscountedPrice = (price: number, discountRate?: number) => {
     if (discountRate) {
       const discountAmount = price * (discountRate / 100)
       return price - discountAmount
+      // 할인가 계산
     }
     return price
   }
