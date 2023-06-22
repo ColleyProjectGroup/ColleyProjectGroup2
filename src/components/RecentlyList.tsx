cd58c17dd48e499b70cac53d0ac666de5de4fdf1
EungBug <eungb14@gmail.com>
2023-06-22 14:46:47 +0900

    Feat: 메인페이지 좌측 뱃지 구현

--- src/components/RecentlyList.tsx
+++ src/components/RecentlyList.tsx
@@ -1,52 +1,60 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Product } from 'types/index'
 import SwiperCore, { Navigation } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Link } from 'react-router-dom'
 import styled from 'styles/components/recentlyList.module.scss'
 import 'swiper/scss'
 import 'swiper/scss/navigation'
 
 export const RecentlyList = React.memo(
   ({ products }: { products: Product[] }) => {
+    const [isObserver, setIsObserver] = useState<boolean>(false)
     const [swiperIndex, setSwiperIndex] = useState(0)
     const navigationPrevRef = React.useRef(null)
     const navigationNextRef = React.useRef(null)
+    useEffect(() => {
+      if (navigationNextRef.current && navigationPrevRef.current) {
+        setIsObserver(true)
+      } else {
+        setIsObserver(false)
+      }
+    }, [navigationPrevRef, navigationNextRef])
 
     SwiperCore.use([Navigation])
 
     return (
       <div className={styled.recently}>
         <p>{`RECENTLY\nVIEWED`}</p>
-        <Swiper
-          direction="vertical"
-          className={styled.swiper}
-          navigation={{
-            prevEl: navigationPrevRef.current,
-            nextEl: navigationNextRef.current
-          }}
-          spaceBetween={0}
-          slidesPerView={4}
-          initialSlide={0}
-          onActiveIndexChange={swiperCore => {
-            console.log(swiperCore)
-            setSwiperIndex(swiperCore.activeIndex)
-          }}
-          // onSlideChange={() => console.log('slide change')}
-          // onSwiper={swiper => console.log(swiper)}
-        >
-          {products.map(product => {
-            return (
-              <SwiperSlide
-                className={styled.thumb}
-                key={product.id}>
-                <img
-                  src={product.thumbnail}
+        {isObserver ? (
+          <Swiper
+            direction="vertical"
+            className={styled.swiper}
+            navigation={{
+              prevEl: navigationPrevRef.current,
+              nextEl: navigationNextRef.current
+            }}
+            height={310}
+            spaceBetween={10}
+            slidesPerView={4}
+            initialSlide={0}
+            autoHeight={false}>
+            {products.map(product => {
+              return (
+                <SwiperSlide
                   className={styled.thumb}
-                />
-              </SwiperSlide>
-            )
-          })}
-        </Swiper>
+                  key={product.id}>
+                  <Link to={`/products/${product.id}`}>
+                    <img
+                      src={product.thumbnail}
+                      className={styled.thumb}
+                    />
+                  </Link>
+                </SwiperSlide>
+              )
+            })}
+          </Swiper>
+        ) : null}
         <div className={styled.navigation}>
           <div ref={navigationPrevRef}>{'<'}</div>
           <div ref={navigationNextRef}>{'>'}</div>
