54df187c97f014bfd0a9a809b01e21f5b0de5eb7
hookor <gerrard1219@gmail.com>
2023-06-23 20:20:43 +0900

    Priceinfo & Productinfo Layout

--- src/components/payment/PriceInfo.tsx
+++ src/components/payment/PriceInfo.tsx
@@ -1,9 +1,31 @@
 import styles from 'src/styles/components/payment/PriceInfo.module.scss'
+import { useLocation } from 'react-router-dom'
+
 export const PriceInfo = () => {
+  const receipt = useLocation().state
   return (
-    <div className={styles.container}>
-      <h3>결제 정보</h3>
-      <div>PriceInfo/queryItems Sum</div>
-    </div>
+    <>
+      <div className={styles.container}>
+        <h3>결제 정보</h3>
+        <div className={styles.wrapper}>
+          <div className={styles.block}>
+            <span>주문상품</span>
+            <span className={styles.numbers}>price</span>
+          </div>
+          <div className={styles.block}>
+            <span>배송비</span>
+            <span className={styles.numbers}>3000</span>
+          </div>
+          <div className={styles.block}>
+            <span>할인/부가결제</span>
+            <span className={styles.discount}>0</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.sum}>
+        <span className={styles.total}>최종 결제 금액</span>
+        <span className={styles.totalprice}>5,000원</span>
+      </div>
+    </>
   )
 }
--- src/components/payment/ProductInfo.tsx
+++ src/components/payment/ProductInfo.tsx
@@ -1,9 +1,25 @@
 import styles from 'src/styles/components/payment/ProductInfo.module.scss'
+import { useLocation } from 'react-router-dom'
+
 export const ProductInfo = () => {
+  const chosenProduct = useLocation().state
+  console.log(chosenProduct.thumbnail)
   return (
     <div className={styles.container}>
       <h3>상품정보</h3>
-      <div>ProductInfo/바로구매 클릭시 /payment?query</div>
+      <div className={styles.wrapper}>
+        <img
+          src={chosenProduct.thumbnail}
+          alt=""
+        />
+        <div className={styles.info}>
+          <span className={styles.title}>{chosenProduct.title}</span>
+          <span className={styles.quantity}>
+            수량: {chosenProduct.quantity}개
+          </span>
+          <span className={styles.price}>{chosenProduct.price}원</span>
+        </div>
+      </div>
     </div>
   )
 }
