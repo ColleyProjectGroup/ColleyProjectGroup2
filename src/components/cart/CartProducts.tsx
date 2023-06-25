7ae024141da42f938320aead1e0cb42cec4bea7d
hookor <gerrard1219@gmail.com>
2023-06-25 22:45:54 +0900

    Cart layout

--- src/components/cart/CartProducts.tsx
+++ src/components/cart/CartProducts.tsx
@@ -10,8 +10,16 @@ export const CartProducts = () => {
           <h4 className={styles.selected}>일반상품{}</h4>
           {/* MAP */}
           <CartItem />
-          <div className={styles.summary}></div>
+          <div className={styles.summary}>
+            <h5>[기본배송]</h5>
+            <div>
+              상품구매금액 {} + 배송비 {}
+            </div>
+            <div>합계 : {}원</div>
+          </div>
+          <a>전체선택</a>
         </div>
+
         <CartSummary />
       </div>
     </>
