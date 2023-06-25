3bba8c9f8f40c4bd039c41d74b49b4fa95bda44d
hookor <gerrard1219@gmail.com>
2023-06-26 01:39:31 +0900

    Product numbers

--- src/components/cart/CartProducts.tsx
+++ src/components/cart/CartProducts.tsx
@@ -12,7 +12,7 @@ export const CartProducts = () => {
         <div className={styles.wrapper}>
           <div className={styles.heading}>장바구니 상품</div>
           {/* PRODUCTS QUANTITY */}
-          <h4 className={styles.selected}>일반상품{}</h4>
+          <h4 className={styles.selected}>일반상품 ({userCart.length})</h4>
           {/* MAP */}
           {userCart.map(product => (
             <CartItem product={product} />
