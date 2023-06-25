e9891b9574c2d6d5a5c2388a17c9e3762b943d8e
hookor <gerrard1219@gmail.com>
2023-06-25 22:24:52 +0900

    Products heading

--- src/components/cart/CartProducts.tsx
+++ src/components/cart/CartProducts.tsx
@@ -5,6 +5,9 @@ export const CartProducts = () => {
     <>
       <div className={styles.container}>
         <div className={styles.wrapper}>
+          <div className={styles.heading}>장바구니 상품</div>
+          {/* PRODUCTS QUANTITY */}
+          <h4 className={styles.selected}>일반상품{}</h4>
           {/* MAP */}
           <CartItem />
           <div className={styles.summary}></div>
