f38754da375c9f44dcd3a256b4218bb082ddbcf1
hookor <gerrard1219@gmail.com>
2023-06-25 17:26:59 +0900

    Header steps

--- src/components/cart/CartHeader.tsx
+++ src/components/cart/CartHeader.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom'
+import styles from 'styles/components/cart/cartHeader.module.scss'
 export const CartHeader = () => {
   return (
-    <div>CartHeader</div>
+    <div>
+      <div className={styles.path}>
+        <Link to="/">홈 /</Link>
+        <strong> 장바구니</strong>
+      </div>
+      <h2>장바구니</h2>
+      <ol className={styles.step}>
+        <li className={styles.selected}>장바구니</li>
+        <li className={styles.selected}>주문서작성</li>
+        <li>주문완료</li>
+      </ol>
+    </div>
   )
 }
