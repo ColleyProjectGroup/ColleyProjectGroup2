51a6dc559589830f5b41c4d4d35bfafe4c5d4953
EungBug <eungb14@gmail.com>
2023-06-29 13:12:20 +0900

    Fix: Order Page 로그인 체크 추가

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { TransactionDetail } from 'types/index'
 import { featchUserOrders } from 'api/index'
 import { MyOrderList } from 'components/index'
 import { Link } from 'react-router-dom'
+import { LoginContext } from 'contexts/index'
 import styled from 'styles/pages/myOrders.module.scss'
 
 export const Order = () => {
+  const { isLogined } = useContext(LoginContext)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [orders, setOrders] = useState<TransactionDetail[]>([])
 
@@ -24,27 +26,30 @@ export const Order = () => {
     fetchOrders()
   }, [])
 
-  return (
-    <div className={styled.container}>
-      <div className={styled.path}>
-        <Link to="/">홈</Link>
-        <span>/</span>
-        <Link to="/mypage">마이쇼핑</Link>
-        <span>/ 주문조회</span>
-      </div>
-      <h4>주문 조회</h4>
-      <div className={styled.tabs}>
-        <div className={`${styled.tab} ${styled.active}`}>
-          주문내역조회 ({orders.length.toLocaleString()})
+  return
+  {
+    isLogined ? (
+      <div className={styled.container}>
+        <div className={styled.path}>
+          <Link to="/">홈</Link>
+          <span>/</span>
+          <Link to="/mypage">마이쇼핑</Link>
+          <span>/ 주문조회</span>
         </div>
-        <div className={`${styled.tab} ${styled.inactive}`}></div>
+        <h4>주문 조회</h4>
+        <div className={styled.tabs}>
+          <div className={`${styled.tab} ${styled.active}`}>
+            주문내역조회 ({orders.length.toLocaleString()})
+          </div>
+          <div className={`${styled.tab} ${styled.inactive}`}></div>
+        </div>
+        <MyOrderList
+          title={''}
+          orders={orders}
+          onFetch={fetchOrders}
+          isLoading={isLoading}
+        />
       </div>
-      <MyOrderList
-        title={''}
-        orders={orders}
-        onFetch={fetchOrders}
-        isLoading={isLoading}
-      />
-    </div>
-  )
+    ) : null
+  }
 }
